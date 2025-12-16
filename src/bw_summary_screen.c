6d8d3194046a0b193e561642f1d922e2c2dc2fb9
PCG06 <pcggaming06@gmail.com>
2025-12-16 18:46:14 +0530

    Fix relearner defaulting to level up moves when there are no moves to teach (only visual)

--- src/bw_summary_screen.c
+++ src/bw_summary_screen.c
@@ -2542,8 +2542,11 @@ static void Task_HandleInput(u8 taskId)
             if (P_SUMMARY_SCREEN_MOVE_RELEARNER && (sMonSummaryScreen->currPageIndex == PSS_PAGE_BATTLE_MOVES || sMonSummaryScreen->currPageIndex == PSS_PAGE_CONTEST_MOVES) && !gMain.inBattle)
             {
                 TryUpdateRelearnType(TRY_INCREMENT);
-                PlaySE(SE_SELECT);
-                ShowMoveRelearner();
+                if (ShouldShowMoveRelearner())
+                {
+                    PlaySE(SE_SELECT);
+                    ShowMoveRelearner();
+                }
             }
         }
         else if (JOY_NEW(L_BUTTON)) // L means decrease. Level <- Egg <- TM <- Tutor
@@ -2551,8 +2554,11 @@ static void Task_HandleInput(u8 taskId)
             if (P_SUMMARY_SCREEN_MOVE_RELEARNER && (sMonSummaryScreen->currPageIndex == PSS_PAGE_BATTLE_MOVES || sMonSummaryScreen->currPageIndex == PSS_PAGE_CONTEST_MOVES) && !gMain.inBattle)
             {
                 TryUpdateRelearnType(TRY_DECREMENT);
-                PlaySE(SE_SELECT);
-                ShowMoveRelearner();
+                if (ShouldShowMoveRelearner())
+                {
+                    PlaySE(SE_SELECT);
+                    ShowMoveRelearner();
+                }
             }
         }
     }
